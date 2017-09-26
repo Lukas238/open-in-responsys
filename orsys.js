24e1d38da79a053b397e8bd5444b416d5fc51c04
Lucas Dasso <Dasso.Lucas@wunderman.com>
2017-09-26 16:06:44 -0300

    Added an init command for configuration. Added context menu command for add and remove in windows.

--- orsys.js
+++ orsys.js
@@ -1,82 +1,144 @@
 #!/usr/bin/env node
 
+
+/**
+ *  PACKAGES
+*/
+
 var program = require('commander'),
 	path      = require('path'),
 	fs      = require('fs'),
 	open    = require("open");
 
-// Get current file name and path
-program.arguments('<file>')
-	.option('-e, --edit', 'Open the file in edit an windows in Responsys')
-	.option('-b, --browser <browserName>', '(optional) the application to be used to open the file (for example, "chrome", "firefox")')
-	.action(function(file) {
-		
-		[paths, filename] = parseFilename(file); //Parse filename and path
-		
-		//Look for rsys.ini in array of paths.
-		var rsys_path = get_rsys_path(paths);
-		
-		if( !rsys_path ){
-			return;
-		}
-		
-		var baseURL = 'https://interact2.responsys.net/interact/formview/doit?';
-		if( program.edit ){
-			baseURL = 'https://interact2.responsys.net/interact/formcab/FileEdit?uid=';
-		}
-		
-		
-		open_browser_url(baseURL + rsys_path + '^' + filename);
-		
-	})
-	.parse(process.argv);
+/**
+ *  VARIABLES
+*/
+var sep = path.sep;
+var baseURL = 'https://interact2.responsys.net/interact/';
+var view_url = 'formview/doit?';
+var edit_url = 'formcab/FileEdit?uid=';
+var file = false;
 
+/**
+ *  FUNCTIONS
+*/
 
-	
 // Return an array with an array of folder directories, and the filename as string.
 function parseFilename(file){
-	var arr_filepath = file.split(path.sep);
-	return [
-		arr_filepath.slice(0, -1), // Array of path sans filename
-		arr_filepath.slice(-1)[0] //Filename string sans path
-	];
-};
+	var arr_filepath = file.split(sep);
 
+	if(arr_filepath.length == 1){
+		return [
+			__dirname.split(sep), // Array of path sans filename
+			file //Filename string sans path
+		];
+	}else{
+		return [
+			arr_filepath.slice(0, -1), // Array of path sans filename
+			arr_filepath.slice(-1)[0] //Filename string sans path
+		];
+	}
+}
+
+/* Search and read "orsys.json" config file */
 function get_rsys_path(paths){
-	
 	for(var i = paths.length; i >= 1; i--){
 		var current_relative_path = paths.slice(i).join('/');
 		current_relative_path += current_relative_path ? '/' : '';
-		var search_path = paths.slice(0, i).join(path.sep) + path.sep;
-	
-		var data = get_rsys_data(search_path);
-		
+		var search_path = paths.slice(0, i).join(sep) + sep;
+		var data = get_rsys_data(search_path);	
 		if(data){
 			return data.root + '/' + current_relative_path;
 		}
 	}
-	
-	console.log("File 'orsys.json' not found!");
-	
+	console.log('Missing "orsys.json" configuration file.\n\nType "orsys --help" for help.');
 	return false;
 }
-
-function get_rsys_data(path){
-	var filepath = path + 'orsys.json';
+/* Open and parse "orsys.json" config file */
+function get_rsys_data(paths){
+	var filepath = paths + 'orsys.json';
 	try {
 	  fileContents = fs.readFileSync(filepath);
 	} catch (err) {
 	  return false;
 	}
-	
 	var obj = JSON.parse(fileContents, 'utf8');
 	return obj;
 }
 
-
-
-// Open the path in browser
+/* Open the path in browser */
 function open_browser_url(url){
 	var browser = program.browser || false;
 	open(url, browser);
 }
+
+function writeConfigFile(){
+	var obj = {
+		root: typeof program.initialize == 'boolean' ? 'contentlibrary' : program.initialize
+	};	
+	var json = JSON.stringify(obj);
+	fs.writeFile('orsys.json', json, 'utf8');	
+	console.log('"orsys.json" config file successfully created.');
+}
+
+function contextMenu(add){
+	var exec = require('child_process').exec;
+	if(add){
+		exec('cmd /c orsys_menu_add.reg');
+	}else{
+		exec('cmd /c orsys_menu_remove.reg');
+	}
+}
+
+/**
+ *  SCRIPT
+*/
+program.arguments('<file>')
+	.option('-e, --edit', 'Open the file in the edit windows in Responsys.')
+	.option('-b, --browser <browserName>', 'Define the browser to used to open the file (ex.: "chrome", "firefox").', '')
+	.option('-i, --initialize [ResponsysRoot]', 'Create a "orsys.json" configuration file in current folder.')
+	.option('-a, --win-contextmenu-add', 'Add the "Responsys" contextual menu. Win only.')
+	.option('-r, --win-contextmenu-remove', 'Remove "Responsys" contextual menu. Win only.')
+	.action(function(param_file) {
+		file = param_file;
+		
+	})
+	.parse(process.argv);
+	
+	// console.log(JSON.stringify(program));
+
+	//Create a configfile
+	if(program.initialize){
+		writeConfigFile();
+		return;
+	}
+	
+	//add contextual menu in Windows
+	if(program.winContextmenuAdd){
+		contextMenu(true);
+		return;
+	}
+	
+	//remove contextual menu in Windows
+	if(program.winContextmenuRemove){
+		contextMenu();
+		return;
+	}
+	
+	// Check for a valid filename
+	if(!file){
+		console.log('Missing file path.\n\nType "orsys --help" for help.');
+		return false;
+	}
+
+	// Parse filename
+	[paths, filename] = parseFilename(file); //Parse filename and path
+	
+	//Look for configfile.
+	var rsys_paths = get_rsys_path(paths);
+	if( !rsys_paths ){
+		return;
+	}
+
+	//Open URL un browser
+	open_browser_url(baseURL + ( program.edit ? edit_url : view_url ) + rsys_paths + '^' + filename);
